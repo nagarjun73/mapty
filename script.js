1f582abba6caefae84e1db7b9f4811d0a8def422
Nagarjun <111555377+nagarjun7397@users.noreply.github.com>
2022-12-19 17:55:12 +0530

    added marker and markrt popup

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+navigator.geolocation.getCurrentPosition(function(position){
+    const {latitude} = position.coords
+    const {longitude} = position.coords
+
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView(coords, 13);
+    // console.log(map)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    map.on('click', function(mapEvent){
+        const { lat, lng} = mapEvent.latlng;
+        console.log(lat, lng)
+        L.marker([lat, lng]).addTo(map)
+            .bindPopup(
+                L.popup({
+                maxWidth:250,
+                minWidth:100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+            ).setPopupContent('workout')
+            .openPopup()
+    })
+}, function() {
+    alert("position not located")
+})
