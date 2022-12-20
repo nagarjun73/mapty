5fe9903557fb4db9644b393b23b59152970078d7
Nagarjun <111555377+nagarjun7397@users.noreply.github.com>
2022-12-20 19:19:59 +0530

    creating workout class to manege workout data

--- script.js
+++ script.js
@@ -13,6 +13,53 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 
 
+class Workout {
+    date = new Date();
+    id = (Date.now() + "").slice(-10);
+
+    constructor(coords, distance,duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration,cadence){
+        super(coords,distance, duration);
+        this.cadence = cadence;
+        this.calcPace();
+    }
+
+    calcPace(){
+        // min/km
+
+        this.pace = this.duration/this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration,elevationGain){
+        super(coords,distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+        }
+
+    calcSpeed(){
+        // km/hr
+
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed;
+
+    }
+}
+
+const speed = new Cycling([null, null], 35, 75, null)
+console.log(speed)
+
+
+//APP ARCHITECTURE
 class App {
     #map;
     #mapEvent;
