40b303bfd10b6855c776bd43a2c5dc8013ad161d
Nagarjun <111555377+nagarjun7397@users.noreply.github.com>
2022-12-20 11:33:36 +0530

    added workout form and toggle feature in it

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map;
+let mapEvent;
 
 navigator.geolocation.getCurrentPosition(function(position){
     const {latitude} = position.coords
@@ -18,27 +20,45 @@ navigator.geolocation.getCurrentPosition(function(position){
 
     const coords = [latitude, longitude]
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
     // console.log(map)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    map.on('click', function(mapEvent){
-        const { lat, lng} = mapEvent.latlng;
-        console.log(lat, lng)
-        L.marker([lat, lng]).addTo(map)
-            .bindPopup(
-                L.popup({
+    map.on('click', function(mapE){
+        mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
+
+
+}, function() {
+    alert("position not located")
+})
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    //Display Marker
+    const { lat, lng} = mapEvent.latlng;
+    console.log(lat, lng)
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(
+            L.popup({
                 maxWidth:250,
                 minWidth:100,
                 autoClose: false,
                 closeOnClick: false,
                 className: 'running-popup',
             })
-            ).setPopupContent('workout')
-            .openPopup()
-    })
-}, function() {
-    alert("position not located")
+        ).setPopupContent('workout')
+        .openPopup()
 })
+})
+
+inputType.addEventListener("change", function() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+
+})
+
